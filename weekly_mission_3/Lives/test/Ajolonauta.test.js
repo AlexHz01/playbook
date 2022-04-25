bcd99132f716f186d4974a161c0a60785121f8a9
AlexHz01 <yagamilonzoro@gmail.com>
2022-04-25 14:14:32 -0500

    Agregando  segundo test

--- weekly_mission_3/Lives/test/Ajolonauta.test.js
+++ weekly_mission_3/Lives/test/Ajolonauta.test.js
@@ -2,7 +2,7 @@ const Ajolonauta = require ('./../app/Ajolonauta')
 
     describe("Prueba de unidad de ajolonauta",() => {
         test('caso de prueba 1: Creacion de objetos', () =>{
-            // aqui puedes colocar el codigo como lo vas u tilizar
+            // aqui puedes colocar el código como lo vas u tilizar
             const woopa = new Ajolonauta("woopa")
 
             // validar el resultado esperado
