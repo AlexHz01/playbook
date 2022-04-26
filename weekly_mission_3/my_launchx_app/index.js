3ebb07443a8e639bfa4b94a4625ca55820233fb4
AlexHz01 <yagamilonzoro@gmail.com>
2022-04-26 13:36:26 -0500

    Creando mi primera prueba de test con jest

--- weekly_mission_3/my_launchx_app/index.js
+++ weekly_mission_3/my_launchx_app/index.js
@@ -1,3 +1,4 @@
-const MissionCommander = requiere('./app/missionCommander');
+const MissionCommander = require ('./app/missionCommander');
 const kike = new MissionCommander ("Kike")
 console.log(kike.name)
+
