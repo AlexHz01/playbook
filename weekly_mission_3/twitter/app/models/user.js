5c6648739469fd299d7b1de11424fed070f2aeca
AlexHz01 <yagamilonzoro@gmail.com>
2022-04-28 13:35:15 -0500

    Agregando test

--- weekly_mission_3/twitter/app/models/user.js
+++ weekly_mission_3/twitter/app/models/user.js
@@ -1,10 +1,29 @@
 class user {
-   constructor (ID, userName, bio, dateCreated, lastUpdated ){
+   constructor (ID, userName, name , bio, dateCreated, lastUpdated ){
         this.ID = ID
         this.userName = userName
+        this.name = name
         this.bio = bio
         this.dateCreated = new Date()
         this.lastUpdated = new Date()
+    }
+    get getUsername () {
+        return  `My name is ${this.userName}`
+    }
+    get getBio() {
+        return this.bio
+    }
+    get getDateCreated() {
+        return this.dateCreated
+    }
+    get getLastUpdated() {
+        return this.lastUpdated
+    }
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+    set setBio(NewBio){
+        this.bio = NewBio
     }
 }
 
--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,15 +1,35 @@
 const User = require('../../app/models/user')
 
 describe('Unit Test para usar clases',() => {
-test('Crear un objeto de usuario', () => {
-    const user = new User  (1, "Kike", "Bio", "Datecreated", "LastUpdated" )
+  test('Crear un objeto de usuario', () => {
+    const user = new User  (1, "Riusey", "kike", "Bio", "Datecreated", "LastUpdated" )
    expect(user.ID).toBe(1)
-   expect(user.userName).toBe("Kike")
+   expect(user.userName).toBe("Riusey")
    expect(user.bio).toBe("Bio")
    expect(user.dateCreated).not.toBeUndefined()
    expect(user.lastUpdated).not.toBeUndefined()
    
+  });
+  
+  test('2da Prueba de usuario agregando get', () => {
+    const user2 = new User  (1, "Riusey", "kike", "Bio")
+    expect(user2.ID).toBe(1)
+    expect(user2.getUsername).toBe("My name is Riusey")
+    expect(user2.bio).toBe("Bio")
+    expect(user2.getDateCreated).not.toBeUndefined()
+    expect(user2.getLastUpdated).not.toBeUndefined()
 
-});
+  });
+
+  test('3ea prueba agregando setters', () =>{
+      const user3 = new User(1,"Riusey","kike","Bio")
+      user3.setUsername = "Alex"
+      expect (user3.username).toBe("Alex")
+
+      user3.setBio = "New Bio"
+      expect (user3.bio).toBe("New Bio")
+  });
+
+
 
 })
